07f0264dc46ffc30fdd792412b4ec839524fb1f3
Chris Kimpton <chris@kimptoc.net>
2017-08-27 11:03:08 +0100

    cors issue?

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
 $(function() {
 
-    console.log("loaded2!");
+    console.log("loading...");
+
+    var rss_feed = "http://feeds.pinboard.in/rss/secret:9a854780513e93dfab88/u:kimptoc/t:mysites/";
+
+    RSSParser.parseURL(rss_feed, function(err, parsed) {
+      console.log(parsed.feed.title);
+      parsed.feed.entries.forEach(function(entry) {
+        console.log(entry.title + ':' + entry.link);
+      })
+    })
 
 });
